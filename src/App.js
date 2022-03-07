050a9616f4e722d82791e78f5f18d5f8f2d48286
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-07 15:58:04 +0100

    Add components to app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import Header from './components/Header';
+import List from './components/List';
+
 export default function App() {
-  return <div>Hello World!</div>;
+  return (
+    <>
+      <Header text="Top 10 to do in Hamburg" />
+      <List />
+    </>
+  );
 }
