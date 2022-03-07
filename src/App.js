372c58ac9c8088a8f35d79bb9dbaf52d13eaa589
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-07 17:59:27 +0100

    Delete component header as not really needed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import Header from './components/Header';
+import styled from 'styled-components';
 import List from './components/List';
 
 export default function App() {
   return (
     <>
-      <Header text="Top 10 to do in Hamburg" />
+      <Title>Top 10 to do in Hamburg</Title>
       <List />
     </>
   );
 }
+
+const Title = styled.h1`
+  background: #f0e7da;
+  padding: 10px;
+  font-size: 30px;
+  text-align: center;
+  text-transform: uppercase;
+  color: rgba(71, 39, 35, 0.72);
+`;
