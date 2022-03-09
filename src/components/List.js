2801286de3790530be94cb94d74998fa97575e2b
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-09 09:05:09 +0100

    Add scrollIntoView with error message when list is empty

--- src/components/List.js
+++ src/components/List.js
@@ -1,13 +1,29 @@
 import styled from 'styled-components';
 import Card from './Card';
 import { nanoid } from 'nanoid';
+import { useEffect, useRef } from 'react';
 
 export default function List({ activities }) {
+  const activitiesEndRef = useRef(null);
+  const scrollToBottom = () => {
+    activitiesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+  useEffect(scrollToBottom, [activities]);
+
+  if (!activities || activities.length === 0) {
+    return (
+      <ListStyle role="list">
+        <li> nothing yet</li>
+        <div ref={activitiesEndRef} />
+      </ListStyle>
+    );
+  }
   return (
     <ListStyle role="list">
       {activities.map(activity => (
         <li key={nanoid()}>
           <Card activity={activity.activity} friend={activity.friend} />
+          <div ref={activitiesEndRef} />
         </li>
       ))}
     </ListStyle>
