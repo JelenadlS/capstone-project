d7fc3bea005cc4e593a7b8061fe086ca0c10b426
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-12 10:37:00 +0100

    DeleteModal test not working

--- src/components/DeleteModal.spec.js
+++ src/components/DeleteModal.spec.js
@@ -1,9 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import DeleteModal from './DeleteModal.js';
-import userEvent from '@testing-library/user-event';
 
 describe('DeleteModal', () => {
   it('renders a p element with text and two buttons', () => {
     render(<DeleteModal />);
+
+    const text = screen.getByText('Are you sure you want to delete?');
+    const keepButton = screen.getByRole('button', {
+      name: /no, i wanna keep it/i,
+    });
+    const deleteButton = screen.getByRole('button', {
+      name: /please delete/i,
+    });
+    expect(text).toBeInTheDocument();
+    expect(keepButton).toBeInTheDocument();
+    expect(deleteButton).toBeInTheDocument();
   });
 });
