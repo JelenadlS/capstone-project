ce03dd319bea27521d9f132b4d7cde3fd216e192
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-12 11:48:08 +0100

    Finalize testing modal

--- src/components/DeleteModal.spec.js
+++ src/components/DeleteModal.spec.js
@@ -1,10 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import DeleteModal from './DeleteModal.js';
+import userEvent from '@testing-library/user-event';
 
 describe('DeleteModal', () => {
   it('renders a p element with text and two buttons', () => {
-    render(<DeleteModal />);
-
+    render(<DeleteModal show={true} />);
     const text = screen.getByText('Are you sure you want to delete?');
     const keepButton = screen.getByRole('button', {
       name: /no, i wanna keep it/i,
@@ -15,5 +15,29 @@ describe('DeleteModal', () => {
     expect(text).toBeInTheDocument();
     expect(keepButton).toBeInTheDocument();
     expect(deleteButton).toBeInTheDocument();
+  });
+
+  it('when clicking please delete, the current card will be deleted', () => {
+    const deleteFunctionality = jest.fn();
+    render(<DeleteModal show={true} onDelete={deleteFunctionality} />);
+
+    const deleteButton = screen.getByRole('button', {
+      name: /please delete/i,
+    });
+
+    userEvent.click(deleteButton);
+    expect(deleteFunctionality).toHaveBeenCalled();
+  });
+
+  it('when clicking NO, the current card will be kept', () => {
+    const keepFunctionality = jest.fn();
+    render(<DeleteModal show={true} onClose={keepFunctionality} />);
+
+    const keepButton = screen.getByRole('button', {
+      name: /no, i wanna keep it/i,
+    });
+
+    userEvent.click(keepButton);
+    expect(keepFunctionality).toHaveBeenCalled();
   });
 });
