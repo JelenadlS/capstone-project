dc26d5728696742ba135a4aa033d4a715c3c077f
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-13 09:39:30 +0100

    Add MemoryRouter to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
 import GlobalStyles from '../src/GlobalStyles';
-
+import {MemoryRouter} from 'react-router-dom';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -14,10 +14,10 @@ export const parameters = {
 export const decorators = [
   Story => (
     <>
-
+    <MemoryRouter>
     <GlobalStyles />
     <Story />
- 
+    </MemoryRouter>
     </>
   )
 ]
