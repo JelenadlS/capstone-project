797c88b89e3d322a918719b2246bf4d1565aff1f
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-13 17:00:06 +0100

    Add storybook to header

--- src/components/Header.stories.js
+++ src/components/Header.stories.js
@@ -1,23 +1,19 @@
 import Header from './Header.js';
-import GlobalStyles from '../GlobalStyles.js';
 
 export default {
   title: 'Component/Header',
   component: Header,
-  decorators: [
-    Story => (
-      <>
-        <GlobalStyles />
-        <Story />
-      </>
-    ),
-  ],
 };
 
 const Template = args => <Header {...args} />;
 
-export const HeaderAB = Template.bind({});
-HeaderAB.args = {};
+export const Default = Template.bind({});
+Default.args = {
+  title: 'my activities',
+};
 
-export const HeaderXY = Template.bind({});
-HeaderXY.args = {};
+export const HeaderWArrow = Template.bind({});
+HeaderWArrow.args = {
+  title: 'Spazieren',
+  link: 'y',
+};
