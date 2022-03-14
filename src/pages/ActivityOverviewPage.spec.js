d4b0da0bcc10c3e84a700b2d2a4558a99beb6e28
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-14 17:44:01 +0100

    Finalize form test

--- src/pages/ActivityOverviewPage.spec.js
+++ src/pages/ActivityOverviewPage.spec.js
@@ -3,22 +3,33 @@ import { MemoryRouter } from 'react-router-dom';
 import ActivityOverviewPage from './ActivityOverviewPage';
 
 describe('ActivityOverviewPage', () => {
-  it('renders ActivityOverviewPage with the backButton with the link, activity twice (header and text), the friend', () => {
-    const data = [{ activity: 'Frau Möller' }, { friend: 'Clara' }];
+  it('renders ActivityOverviewPage with the backButton with the link, activity twice (header and text), the friend, notes, date and location', () => {
     render(
       <MemoryRouter>
-        <ActivityOverviewPage data={data} />
+        <ActivityOverviewPage
+          friend="Clara"
+          activity="Frau Möller"
+          notes="notes"
+          date="13/02/21"
+          location="HH"
+        />
       </MemoryRouter>
     );
 
     const arrowButton = screen.getByRole('button');
     const link = screen.getByRole('link');
-    const headerTextAndActivity = screen.getAllByRole('Frau Möller');
-    const friend = screen.getByText('mit: Clara');
+    const headerTextAndActivity = screen.getAllByText('Frau Möller');
+    const friend = screen.getByText('Clara');
+    const notes = screen.getByText('notes');
+    const date = screen.getByText('13/02/21');
+    const location = screen.getByText('HH');
 
     expect(arrowButton).toBeInTheDocument();
     expect(link).toBeInTheDocument();
     expect(headerTextAndActivity).toHaveLength(2);
     expect(friend).toBeInTheDocument();
+    expect(notes).toBeInTheDocument();
+    expect(date).toBeInTheDocument();
+    expect(location).toBeInTheDocument();
   });
 });
