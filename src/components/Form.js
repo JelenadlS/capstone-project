c0fced941b7dba60e1172cf0e3bdede301980403
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-14 11:17:41 +0100

    Add fields in form and on visible page

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,54 @@
 import styled from 'styled-components';
 import Button from './Button';
+import { useForm } from 'react-hook-form';
 
 export default function Form({ onAddActivity }) {
-  function handleSubmit(event) {
-    event.preventDefault();
-    const form = event.target;
-    const { activity, friend } = form.elements;
-    onAddActivity({ activity: activity.value, friend: friend.value });
-    activity.value = '';
-    friend.value = '';
-  }
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => {
+    onAddActivity(data);
+  };
 
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   const form = event.target;
+  //   const { activity, friend } = form.elements;
+  //   onAddActivity({ activity: activity.value, friend: friend.value });
+  //   activity.value = '';
+  //   friend.value = '';
+  // }
   return (
     <WrapperForm
       title="add activities"
       autoComplete="off"
-      onSubmit={handleSubmit}
+      // onSubmit={handleSubmit}
+      onSubmit={handleSubmit(onSubmit)}
     >
       <label htmlFor="activity">name of activity:</label>
-      <input id="activity" required type="text" name="activity" />
+      <input
+        id="activity"
+        required
+        type="text"
+        name="activity"
+        {...register('activity')}
+      />
+
       <label htmlFor="friend">who should join you?</label>
-      <input id="friend" type="text" name="friend" />
+      <input id="friend" type="text" name="friend" {...register('friend')} />
+
+      <label htmlFor="notes">space for some additional notes...</label>
+      <input id="notes" type="text" name="notes" {...register('notes')} />
+
+      <label htmlFor="date">do you already have a date in mind?</label>
+      <input id="date" type="text" name="date" {...register('date')} />
+
+      <label htmlFor="location">where is the activity taking place?</label>
+      <input
+        id="location"
+        type="text"
+        name="location"
+        {...register('location')}
+      />
+
       <PositionButton type="submit">Add</PositionButton>
     </WrapperForm>
   );
@@ -29,7 +57,7 @@ export default function Form({ onAddActivity }) {
 const WrapperForm = styled.form`
   height: 85vh;
   display: grid;
-  grid-template-rows: repeat(4, 25px) auto;
+  grid-template-rows: repeat(10, 25px) auto;
   align-items: end;
   gap: 10px;
   color: rgba(71, 39, 35, 0.72);
--- src/pages/ActivityOverviewPage.js
+++ src/pages/ActivityOverviewPage.js
@@ -1,13 +1,35 @@
 import Header from '../components/Header';
 import styled from 'styled-components';
 
-export default function ActivityOverviewPage({ activity, friend }) {
+export default function ActivityOverviewPage({
+  activity,
+  friend,
+  notes,
+  date,
+  location,
+}) {
   return (
     <>
       <Header title={activity} link="y"></Header>
       <WrapperCard>
-        <p>{activity}</p>
-        <p>mit: {friend}</p>
+        <p>
+          <strong>{activity}</strong>
+        </p>
+        <p>
+          <strong>with:</strong> {friend}
+        </p>
+        <div>
+          <p>
+            <strong>additional notes:</strong>
+          </p>
+          <p>{notes}</p>
+        </div>
+        <p>
+          <strong>on the:</strong> {date}
+        </p>
+        <p>
+          <strong>at:</strong> {location}
+        </p>
       </WrapperCard>
     </>
   );
