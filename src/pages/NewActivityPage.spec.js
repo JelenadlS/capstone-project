dc0ee473806b183b876b969fc304399d7e4d62b3
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-15 20:53:19 +0100

    Fix newActivityList test

--- src/pages/NewActivityPage.spec.js
+++ src/pages/NewActivityPage.spec.js
@@ -3,19 +3,17 @@ import NewActivityPage from './NewActivityPage.js';
 import { MemoryRouter } from 'react-router-dom';
 
 describe('NewActivityPage', () => {
-  it('renders NewActivityPage with the backButton with the link and the form elements (4 textboxes and button)', () => {
+  it('renders NewActivityPage with the backLink and the form elements (4 textboxes and button)', () => {
     render(
       <MemoryRouter>
         <NewActivityPage />
       </MemoryRouter>
     );
 
-    const arrowButton = screen.getByRole('button', { name: 'go back' });
     const link = screen.getByRole('link', { name: 'go back' });
     const textboxes = screen.getAllByRole('textbox');
     const addButton = screen.getByRole('button', { name: 'Add' });
 
-    expect(arrowButton).toBeInTheDocument();
     expect(link).toBeInTheDocument();
     expect(textboxes).toHaveLength(4);
     expect(addButton).toBeInTheDocument();
