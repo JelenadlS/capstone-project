b4b5aa9736762fb37b04c1c6aced1556fc77ef54
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-15 22:15:00 +0100

    Try testing with find: https://medium.com/@aarling/mocking-a-react-router-match-object-in-your-component-tests-fa95904dcc55

--- src/pages/EditActivityPage.spec.js
+++ src/pages/EditActivityPage.spec.js
@@ -1,10 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import EditActivityPage from './EditActivityPage.js';
+import { renderWithRouterMatch } from '@testing-library/jest-dom/extend-expect';
 
 describe('EditActivityPage', () => {
   it('renders EditActivityPage with the back button and edit link and the form', () => {
-    render(
+    renderWithRouterMatch(
       <MemoryRouter>
         <EditActivityPage />
       </MemoryRouter>
@@ -13,9 +14,14 @@ describe('EditActivityPage', () => {
     const button = screen.getByRole('button');
     const link = screen.getAllByRole('link');
     const form = screen.getAllByRole('from');
+    const param = screen.getByText({
+      route: '/project/ABC123',
+      path: '/project/:id',
+    });
 
     expect(button).toBeInTheDocument();
     expect(link).toHaveLength(1);
     expect(form).toBeInTheDocument();
+    expect(param).toBeInTheDocument('Match id: ABC123');
   });
 });
