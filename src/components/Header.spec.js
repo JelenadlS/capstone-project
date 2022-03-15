3edd9669b98899779e94c20bf8d38d245f2446f2
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-15 20:55:44 +0100

    Fix header test

--- src/components/Header.spec.js
+++ src/components/Header.spec.js
@@ -3,7 +3,7 @@ import { MemoryRouter } from 'react-router-dom';
 import Header from './Header.js';
 
 describe('Header', () => {
-  it('renders header with title and arrowbutton when link===y', () => {
+  it('renders header with title and arrowlink when link===y', () => {
     render(
       <MemoryRouter>
         <Header title="my activities" link={true} />
@@ -11,10 +11,10 @@ describe('Header', () => {
     );
 
     const title = screen.getByText('my activities');
-    const button = screen.getByRole('button');
+    const link = screen.getByRole('link');
 
     expect(title).toBeInTheDocument();
-    expect(button).toBeInTheDocument();
+    expect(link).toBeInTheDocument();
   });
 
   it('renders header only with title when link!==y', () => {
@@ -25,9 +25,9 @@ describe('Header', () => {
     );
 
     const title = screen.getByText('my activities');
-    const button = screen.queryByRole('button');
+    const link = screen.queryByRole('link');
 
     expect(title).toBeInTheDocument();
-    expect(button).not.toBeInTheDocument();
+    expect(link).not.toBeInTheDocument();
   });
 });
