631f8443b69723cc7d72233664cf53e4ad6bae88
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-16 10:20:13 +0100

    Adjust arrowback in activityoverview to my activities

--- src/pages/ActivityOverviewPage.js
+++ src/pages/ActivityOverviewPage.js
@@ -1,19 +1,22 @@
-import { useParams, Link } from 'react-router-dom';
-import Header from '../components/Header';
+import { useNavigate, useParams, Link } from 'react-router-dom';
+import gobackicon from '../images/goback.svg';
 import editicon from '../images/edit.svg';
 import styled from 'styled-components';
 
 export default function ActivityOverviewPage({ activities }) {
+  const navigate = useNavigate();
   const { id } = useParams();
   const selectedActivity = activities.find(activity => activity.id === id);
 
+  // <Arrowback onClick={() => navigate(`/friend/${name}`)}> da will ich hin
   return (
     <>
-      <Header
-        textAlign="left"
-        title={selectedActivity.activity}
-        link="y"
-      ></Header>
+      <Title>
+        <Arrowback onClick={() => navigate(`/friend`)}>
+          <img src={gobackicon} alt="go back" />
+        </Arrowback>
+        <HeaderText>{selectedActivity.activity}</HeaderText>
+      </Title>
       <WrapperCard>
         <p>
           <strong>{selectedActivity.activity}</strong>
@@ -70,6 +73,34 @@ export default function ActivityOverviewPage({ activities }) {
     </>
   );
 }
+
+const Title = styled.header`
+  background: #f0e7da;
+  padding: 10px;
+  text-align: left;
+  text-transform: uppercase;
+  color: rgba(71, 39, 35, 0.72);
+  position: sticky;
+  top: 0px;
+  z-index: 2;
+  height: 60px;
+  overflow: hidden;
+`;
+
+const Arrowback = styled.button`
+  border: none;
+  background: transparent;
+  position: fixed;
+  top: 5px;
+  left: 2px;
+`;
+
+const HeaderText = styled.h1`
+  margin-left: 70px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
 
 const WrapperCard = styled.section`
   color: rgba(71, 39, 35, 0.72);
