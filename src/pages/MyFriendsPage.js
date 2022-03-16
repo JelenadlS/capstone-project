3da46fe47fa7fe0836632c2b2f042fdfbe905b70
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-16 11:21:57 +0100

    Add list with friends and to do on the top

--- src/pages/MyFriendsPage.js
+++ src/pages/MyFriendsPage.js
@@ -1,17 +1,23 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Button from '../components/Button';
 import newicon from '../images/new.svg';
 import styled from 'styled-components';
 
-export default function MyFriendsPage(activities, id) {
+export default function MyFriendsPage({ activities }) {
   return (
     <>
       <WrapperApp>
         <Header title="my friends" />
         <Main>
-          <div>Test</div>
-          <div>Test</div>
+          <ListStyle role="list" title="list of friends">
+            <li>To do</li>
+            {activities.map(activity => (
+              <li key={activity.id}>
+                <strong>{activity.friend}</strong>
+              </li>
+            ))}
+          </ListStyle>
         </Main>
         <Bottom>
           <NavLink to="newactivity">
@@ -37,6 +43,36 @@ const WrapperApp = styled.div`
 const Main = styled.main`
   overflow-y: auto;
 `;
+
+const ListStyle = styled.ul`
+  list-style-type: none;
+  color: rgba(71, 39, 35, 0.72);
+
+  li {
+    padding: 5px;
+  }
+`;
+
+const WrapperCard = styled.section`
+  color: rgba(71, 39, 35, 0.72);
+  border-bottom: 0.5px solid rgba(71, 39, 35, 0.72);
+  display: grid;
+  grid-template-columns: auto auto;
+  align-items: center;
+  overflow: hidden;
+`;
+
+const LinkStyling = styled(Link)`
+  padding: 8px 8px 0;
+  font-size: 18px;
+  color: rgba(71, 39, 35, 0.72);
+  background-color: transparent;
+  text-decoration: none;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
 const Bottom = styled.div`
   background: #f0e7da;
   text-align: center;
