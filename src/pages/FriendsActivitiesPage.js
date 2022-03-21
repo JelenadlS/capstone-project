8ae2e847ce81af577c3258d84965d3d89dbe1a34
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-21 21:06:17 +0100

    Add tags, sorted

--- src/pages/FriendsActivitiesPage.js
+++ src/pages/FriendsActivitiesPage.js
@@ -1,4 +1,5 @@
 import { NavLink, useParams, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 import { ArrowBackButton } from '../components/Button';
 import Header from '../components/Header';
@@ -21,6 +22,23 @@ export default function FriendsActivitiesPage({
   );
   const navigate = useNavigate();
 
+  const eachExistingCategoryOnce = [
+    ...new Set(selectedFriendsActivity.map(activity => activity.category)),
+  ];
+
+  const eachExistingCategorySorted = eachExistingCategoryOnce.sort(function (
+    a,
+    b
+  ) {
+    const firstFriend = a.toLowerCase();
+    const secondFriend = b.toLowerCase();
+
+    if (firstFriend < secondFriend) return -1;
+    if (firstFriend > secondFriend) return 1;
+    return 0;
+  });
+  const categoryTagsAndAll = ['all', ...eachExistingCategorySorted];
+
   return (
     <Picture>
       <Header>
@@ -30,6 +48,11 @@ export default function FriendsActivitiesPage({
         </ArrowBackButton>
       </Header>
       <Main>
+        <ListStyle role="list" title="list of activities">
+          {categoryTagsAndAll.map((category, index) => {
+            return <li key={index}>{category}</li>;
+          })}
+        </ListStyle>
         <List
           activitiesOfSelectedFriend={selectedFriendsActivity}
           errorMessage={hasError}
@@ -58,3 +81,22 @@ export default function FriendsActivitiesPage({
     }
   }
 }
+
+const ListStyle = styled.ul`
+  list-style-type: none;
+  display: flex;
+  flex-wrap: nowrap;
+  gap: 10px;
+  margin: 10px;
+  overflow-x: auto;
+  height: 35px;
+
+  li {
+    background: transparent;
+    border: 2px solid rgba(71, 39, 35, 0.42);
+    border-radius: 20px;
+    padding: 5px;
+    font-size: 16px;
+    white-space: nowrap;
+  }
+`;
