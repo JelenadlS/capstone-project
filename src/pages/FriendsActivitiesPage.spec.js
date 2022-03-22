49268661becdc9c91d33d35d1d6c87fc627da0a8
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-22 12:54:41 +0100

    Adjust friendsactivity page test

--- src/pages/FriendsActivitiesPage.spec.js
+++ src/pages/FriendsActivitiesPage.spec.js
@@ -14,8 +14,10 @@ jest.mock('react-router-dom', () => ({
 Element.prototype.scrollIntoView = jest.fn();
 
 describe('MyFriendsPage', () => {
-  it('renders a page with headertitle, go back and new button, a list and link', () => {
-    const activities = [{ id: '1', activity: 'Frau Möller', friend: 'Clara' }];
+  it('renders a page with headertitle, go back and new button, as well as filter button, a list and link', () => {
+    const activities = [
+      { id: '1', activity: 'Frau Möller', category: 'sport', friend: 'Clara' },
+    ];
 
     render(
       <MemoryRouter>
@@ -25,13 +27,15 @@ describe('MyFriendsPage', () => {
 
     const title = screen.getByText('Clara');
     const goBackButton = screen.getByRole('button', { name: 'go back' });
-    const NewButton = screen.getByRole('button', { name: 'new' });
+    const newButton = screen.getByRole('button', { name: 'new' });
+    const categoryButton = screen.getByRole('button', { name: 'sport' });
     const list = screen.getByRole('list');
     const link = screen.getByRole('link', { name: 'new' });
 
     expect(title).toBeInTheDocument();
     expect(goBackButton).toBeInTheDocument();
-    expect(NewButton).toBeInTheDocument();
+    expect(newButton).toBeInTheDocument();
+    expect(categoryButton).toBeInTheDocument();
     expect(list).toBeInTheDocument();
     expect(link).toBeInTheDocument();
   });
