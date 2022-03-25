c3a1a6220dbb563a5046eda2cc680b69aecf20ef
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-25 15:15:24 +0100

    Add small chenges

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -4,6 +4,7 @@ import backgroundPicture from '../images/backgroundPicture.svg';
 
 const Picture = styled.div`
   background-image: url(${backgroundPicture});
+  background-size: cover;
 
   background-position: 30% 100%;
   @media (max-width: 425px) {
