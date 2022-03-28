c507e4e5f43f6ae2c5d69b6afd52c47f439120b7
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-28 21:43:47 +0200

    Add basic layout and navigation

--- src/pages/AddFriendPage.js
+++ src/pages/AddFriendPage.js
@@ -1,8 +1,40 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
-    import styled from 'styled-components';
-  
-    export default function AddFriendPage() {
-      return <div>AddFriendPage</div>
+import { ArrowBackButton } from '../components/Button';
+import Header from '../components/Header';
+import Main from '../components/Main';
+import Navigation from '../components/Navigation';
+import Picture from '../components/Picture';
 
-    }
-  
+import goBackIcon from '../images/goBackIcon.svg';
+import newIcon from '../images/newIcon.svg';
+
+export default function AddFriendPage({
+  handleResetPage,
+  handleResetPageAndShowArrow,
+}) {
+  const navigate = useNavigate();
+  return (
+    <Picture>
+      <Header handleResetPage={handleResetPage}>
+        Add a friend
+        <ArrowBackButton onClick={() => navigate(-1)}>
+          <img src={goBackIcon} alt="go back" />
+        </ArrowBackButton>
+      </Header>
+      <Main>
+        <div>Test</div>
+      </Main>
+      <Navigation
+        handleResetPage={handleResetPage}
+        handleResetPageAndShowArrow={handleResetPageAndShowArrow}
+      >
+        <Link to="/newactivity">
+          <img src={newIcon} alt="new" />
+        </Link>
+      </Navigation>
+    </Picture>
+  );
+}
