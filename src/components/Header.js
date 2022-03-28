0f34aa92e04773cfa2ab32442398e674feba67d8
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-03-28 21:28:08 +0200

    Remove friend from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,10 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
-import friendIcon from '../images/friendIcon.svg';
-
 export default function Header({ children, handleResetPage }) {
   return (
     <StyledHeader>
       <StyledTitle>{children}</StyledTitle>
-      <StyledNavLinkFriends to="/" onClick={handleResetPage}>
-        <img width="40" height="20" alt="friendsHomeIcon" src={friendIcon} />
-        <StyledDescription>friends</StyledDescription>
-      </StyledNavLinkFriends>
     </StyledHeader>
   );
 }
@@ -33,26 +27,4 @@ const StyledTitle = styled.h1`
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
-`;
-
-const StyledNavLinkFriends = styled(NavLink)`
-  position: fixed;
-  top: 2px;
-  right: 2px;
-  align-self: center;
-  padding-top: 8px;
-  padding-right: 8px;
-  text-decoration: none;
-  display: flex;
-  flex-direction: column;
-
-  &:active {
-    transform: translateY(+8px);
-  }
-`;
-
-const StyledDescription = styled.p`
-  font-size: 10px;
-  color: rgba(71, 39, 35, 0.72);
-  text-align: center;
 `;
