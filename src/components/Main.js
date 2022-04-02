8b9e8b14b342082a2c397a0b85e91f0e3797c860
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-04-02 12:59:44 +0200

    Change backgroud picture format to improve productivity by 22%

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,13 @@
 import styled from 'styled-components';
 
-import backgroundPicture from '../images/backgroundPicture.svg';
+import backgroundPicture from '../images/backgroundPicture.webp';
 
 const Main = styled.main`
   overflow-y: auto;
-
   background-image: url(${backgroundPicture});
   background-size: cover;
   width: 100%;
-
-  background-position: 30% 100%;
-  @media (max-width: 425px) {
-    background-image: url(${backgroundPicture});
-    background-size: cover;
-    background-position: 40% 50%;
-  }
+  background-position: 35% 100%;
 `;
 
 export default Main;
